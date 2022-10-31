36040c62b75348b1721df72caf6cc6fb236c88ba
Jason Anderson <jason@debian-jason.jason.anderson.com>
2022-10-31 14:20:27 -0600

    Almost finished with lab 3

--- lab-routing/dvrouter.py
+++ lab-routing/dvrouter.py
@@ -15,14 +15,119 @@ class DVRouter(BaseHost):
     def __init__(self):
         super(DVRouter, self).__init__()
 
-        self.my_dv = {}
+        self.my_dv = self.create_dv()
         self.neighbor_dvs = {}
+        self.neighbor_ips = {}
 
         self.forwarding_table = ForwardingTable()
 
         self._initialize_dv_sock()
 
         # Do any further initialization here
+
+    def create_dv(self):
+        dv = {}
+        # Go through interfaces and create dv entries with 0 (Add /32 prefix)
+        for intf in self.all_interfaces:
+            if (intf == "lo"):
+                continue
+
+            # Get IP address and add /32 prefix for this lab
+            prefix = self.int_to_info[intf].ipv4_addrs[0] + "/32"
+
+            # Initial distance is 0
+            entry = {}
+            entry["dist"] = 0
+            entry["next_hop"] = self.hostname
+            dv[prefix] = entry
+            
+        return dv
+
+    def update_forwarding_table(self):
+        self.forwarding_table.flush()
+
+        print(f"Neighbor IP's: {self.neighbor_ips}")
+        # Go through my dv and add next hops to prefixes
+        for prefix in self.my_dv.keys():
+            dv_entry = self.my_dv[prefix]
+
+            if (dv_entry["next_hop"] == self.hostname):
+                continue
+
+            next_hop = self.neighbor_ips[dv_entry["next_hop"]]
+
+            interface = ""
+            # Using next_hop, find interface that it will go out
+            for intf in self.all_interfaces:
+                if (intf == "lo"):
+                    continue
+
+                if (next_hop == self.int_to_info[intf].ipv4_addrs[0]):
+                    interface = intf
+                    break
+
+            # Add entry
+            print(f"Forwarding Table Entry: Prefix {prefix}, Intf: {interface}, Next {next_hop}")
+            self.forwarding_table.add_entry(prefix, interface, next_hop)
+
+
+    # Prefix Map: Prefix ==> {distance: 0, hostname: r3}
+    def populate_prefix_map(self, map, name):
+        print(f"Name: {name}")
+        neighbor_entry = self.neighbor_dvs[name]
+
+        for prefix in neighbor_entry.keys():
+            # If the prefix does not exist, then add it
+            # If it does exist, then check if the distance is less than current
+            #   stored value
+            if (prefix not in map.keys()):
+                entry = {}
+                entry["dist"] = neighbor_entry[prefix]["dist"]
+                entry["next_hop"] = name
+
+                map[prefix] = entry
+            else:
+                if (neighbor_entry[prefix]["dist"] < map[prefix]["dist"]):
+                    map[prefix]["dist"] = neighbor_entry[prefix]["dist"]
+                    map[prefix]["next_hop"] = name
+
+    def bellman_ford(self):
+        # Start from scratch
+        dv = self.create_dv()
+        prefix_map = {}
+
+        # Go through each of immediate neighbors dv's and perform algorithm
+        # print(f"Neighbors: {self.neighbor_dvs}")
+
+        for name in self.neighbor_dvs.keys():
+            self.populate_prefix_map(prefix_map, name)
+
+        # Go through each entry in prefix map and add 1 to all entries (this adds my cost to get there)
+        for prefix in prefix_map.keys():
+            distance = prefix_map[prefix]["dist"]
+            prefix_map[prefix]["dist"] = distance + 1
+
+        # Add default dv stuff into map
+        for key in dv.keys():
+            entry = {}
+            entry["dist"] = dv[key]["dist"]
+            entry["next_hop"] = self.hostname
+            # Potential Bug!!
+            prefix_map[key] = entry
+        
+        return prefix_map
+
+    def create_dv_message(self, intf):
+        msg = {}
+        msg["ip"] = self.int_to_info[intf].ipv4_addrs[0]
+        msg["name"] = self.hostname
+        msg["dv"] = self.my_dv
+
+        # print(f"Creating Message: {msg}")
+
+        msg_str = json.dumps(msg)
+        msg_bytes = msg_str.encode("utf-8")
+        return msg_bytes
 
     def _initialize_dv_sock(self) -> None:
         '''Initialize the socket that will be used for sending and receiving DV
@@ -66,7 +171,30 @@ class DVRouter(BaseHost):
         self.sock.sendto(msg, (dst, DV_PORT))
 
     def handle_dv_message(self, msg: bytes) -> None:
-        pass
+        print("I Got a DV!")
+        # print(f"Message: {msg}")
+
+        # Decode the message from the data
+        obj_str = msg.decode("utf-8")
+        obj = json.loads(obj_str)
+
+        # Grab information from message and store it in associated dv table
+        ip = obj["ip"]
+        name = obj["name"]
+        dv = obj["dv"]
+
+        # Discard packet if it is from me
+        if (name == self.hostname):
+            return
+
+        # Save neighbor IP
+        self.neighbor_ips[name] = ip
+
+        # Save neighbor DV
+        self.neighbor_dvs[name] = dv
+
+        # Update my DV
+        self.update_dv()
 
     def send_dv_next(self):
         '''Send DV to neighbors, and schedule this method to be called again in
@@ -106,7 +234,23 @@ class DVRouter(BaseHost):
         return resolved_dv
 
     def update_dv(self) -> None:
-        pass
+        # print("Updating my DV!")
+        # copy my own dv
+        dv = self.my_dv.copy()
+
+        new_dv = self.bellman_ford()
+        print(f"New DV: {new_dv}")
+
+        # if the new_dv is different than the original then broadcast dv out
+        if (dv != new_dv):
+            # Update forwarding table
+            self.my_dv = new_dv
+            self.update_forwarding_table()
+
+            print("We have a new DV! We will broadcast it out!")
+
+            self.send_dv()
+            
 
     def bcast_for_int(self, intf: str) -> str:
         ip_int = ip_str_to_int(self.int_to_info[intf].ipv4_addrs[0])
@@ -117,3 +261,12 @@ class DVRouter(BaseHost):
 
     def send_dv(self) -> None:
         print('Sending DV')
+
+        # Send my DV to all interfaces (Except loopback)
+        for intf in self.all_interfaces:
+            if (intf == "lo"):
+                continue
+
+            msg = self.create_dv_message(intf)
+
+            self._send_msg(msg, self.bcast_for_int(intf))
