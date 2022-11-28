21695c544a9c87f488726b1c01429e677bb892f1
Jason Anderson <jason@debian-jason.jason.anderson.com>
2022-11-28 14:53:29 -0700

    Send buffer complete

--- lab-tcp-reliable-transport/buffer.py
+++ lab-tcp-reliable-transport/buffer.py
@@ -6,22 +6,47 @@ class TCPSendBuffer(object):
         self.last_seq = self.base_seq
 
     def bytes_not_yet_sent(self):
-        pass
+        return self.last_seq - self.next_seq
 
     def bytes_outstanding(self):
-        pass
+        return self.next_seq - self.base_seq
 
     def put(self, data):
-        pass
+        # Append data to buffer and add to last_seq
+        self.buffer = self.buffer + data
+        self.last_seq = self.last_seq + len(data)
 
     def get(self, size):
-        pass
+        # Check to see if requested size exceeds buffer
+        if (self.next_seq + size > self.last_seq):
+            size = self.last_seq - self.next_seq
+        
+        index = self.next_seq - self.base_seq
+        data = self.buffer[index : (index + size)]
+        seq = self.next_seq
+
+        # Update next_seq
+        self.next_seq = self.next_seq + size
+
+        return (data, seq)
 
     def get_for_resend(self, size):
-        pass
+        if (self.base_seq + size > self.last_seq):
+            size = self.last_seq - self.base_seq
+        
+        index = 0
+        data = self.buffer[index : (index + size)]
+        seq = self.base_seq
+
+        return (data, seq)
 
     def slide(self, sequence):
-        pass
+
+        # Calculate how much to cut off from begining of buffer
+        cutoff_len = sequence - self.base_seq
+        self.buffer = self.buffer[cutoff_len:]
+
+        self.base_seq = sequence
 
 
 class TCPReceiveBuffer(object):
